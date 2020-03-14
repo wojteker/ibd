e5a9ca1e1a8801931f7d7c217b67762f58a78f5c
pawel-zawadzki <pawelz84@gmail.com>
2020-03-14 12:26:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include 'header.php'; ?>
 
-<h1>Witamy w księgarni internetowej</h1>
+<h1>Witamy w Księgarni Internetowej IBD!</h1>
 
 <p>
     Projekt na zaliczenie przedmiotu Internetowe Bazy Danych w roku akademickim <?=ROK_AKADEMICKI ?>.
